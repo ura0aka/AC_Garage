b286b559bc74feb39affdea15c34fc59668739fc
ura0aka <bitcoinassassination@gmail.com>
2024-06-10 10:11:37 -0400

    removed testing for gui

--- src/testing/01-ftxui/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/max/Code/MiniProjects/ACGarage/src/testing/01-ftxui/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/max/Code/MiniProjects/ACGarage/src/testing/01-ftxui/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
